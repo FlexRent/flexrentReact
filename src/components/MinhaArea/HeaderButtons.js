ef366fe63d6faed78d45aa803d40514404791c27
Gustavo Assis <guu.assis@hotmail.com>
2023-05-13 18:56:36 -0300

    ajustando detalhes, e criando background

--- src/components/MinhaArea/HeaderButtons.js
+++ src/components/MinhaArea/HeaderButtons.js
@@ -1,15 +1,20 @@
 import '../../Css/HeaderButtons.css'
 
 export default function HeaderButtons(){ 
-    return ( 
-        <header className='buttons-menu'> 
-        
-            <div className='btn-group d-flex justify-content-between button-group'> 
-                <button type="button" className="button-menu">Meus Produtos</button>
-                <button type="button" className="button-menu">Produtos Locados</button>
-                <button type="button" className="button-menu">Carteira</button>
+    return (
+        <section>
+            <header className='buttons-menu'> 
+                <div className='btn-group d-flex justify-content-between button-group'> 
+                    <button type="button" className="button-menu">Meus Produtos</button>
+                    <button type="button" className="button-menu">Produtos Locados</button>
+                    <button type="button" className="button-menu">Carteira</button>
+                </div>
+            </header>
+            <div className='content'> 
+
             </div>
-        </header>
+        </section>
+
     )
 
 }
