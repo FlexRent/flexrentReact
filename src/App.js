a562765bd611d58a2bb4766c2d43608d987bb28f
Gustavo Assis <guu.assis@hotmail.com>
2023-05-13 18:31:39 -0300

    Botoes de navegacao minha area concluido

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Home from "./components/Home";
 import CadastroProduto from "./components/CadastroProduto";
 import NotFound from "./components/NotFound";
 import CadastroProduto2 from "./components/CadastroProduto2";
+import HeaderButtons from "./components/MinhaArea/HeaderButtons";
 
 function App() {
   return (
-    <Routes>
+    <Routes className="container">
       <Route path="/" element={<Home/>}/>
       <Route path="/cadastro-produto" element={<CadastroProduto/>}/>
       <Route path="/cadastro-produto2" element={<CadastroProduto2/>}/>
+      <Route path="/minha-area" element={<HeaderButtons/>}/>
       <Route path="*" element={<NotFound/>}/>
     </Routes>
+   
   );
 }
 
---
+++ src/components/MinhaArea/HeaderButtons.js
@@ -0,0 +1,15 @@
+import '../../Css/HeaderButtons.css'
+
+export default function HeaderButtons(){ 
+    return ( 
+        <header className='buttons-menu'> 
+        
+            <div className='btn-group d-flex justify-content-between button-group'> 
+                <button type="button" className="button-menu">Meus Produtos</button>
+                <button type="button" className="button-menu">Produtos Locados</button>
+                <button type="button" className="button-menu">Carteira</button>
+            </div>
+        </header>
+    )
+
+}
