823b6a3292b9256bfc85e5359dcef930d9fc3cfe
Gustavo Assis <guu.assis@hotmail.com>
2023-05-14 20:38:37 -0300

    trabalhando no componente de cartoes

--- src/components/MinhaArea/Carteira.js
+++ src/components/MinhaArea/Carteira.js
@@ -3,9 +3,9 @@ import '../../Css/Carteira.css'
 export default function Carteira(){
     return(
         
-        <section className='carteira-content d-flex pt-3'>
+        <section className='carteira-content flex-column pt-3'>
 
-            <div className='saldo row'>
+            <div className='saldo row mb-5'>
 
                 <div className='col-sm-3 d-flex align-items-center justify-content-end' id='saldo-atual'> 
                      <spam>Saldo atual</spam>
@@ -19,6 +19,25 @@ export default function Carteira(){
                     <button>Resgatar</button>
                 </div>
                 
+            </div>
+
+
+            <div className='cartao-content row'> 
+                <div className='titulo ml-2 mt-2'> 
+                    <h5> Meus cartões</h5>
+                </div>
+
+                <div className='cartoes col-sm-6 d-flex align-items-center justify-content-center'> 
+                    <p> Aqui cartoes </p>
+                </div>
+
+                <div className='novo-cartao col-sm-6 d-flex align-items-center justify-content-center '> 
+                    <p> Aqui novo </p>
+                </div>
+
+
+
+
             </div>
          
         </section>
--- src/components/MinhaArea/HeaderButtons.js
+++ src/components/MinhaArea/HeaderButtons.js
@@ -15,7 +15,6 @@ export default function HeaderButtons(){
                <Carteira/>
             </div>
         </section>
-
     )
 
 }
