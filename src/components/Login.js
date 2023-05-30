8c11d24393f304df9535e1a112d5511e374711c9
Rodrigo Rossi <rerossi1985@gmail.com>
2023-05-30 19:38:29 -0300

    ajustes finais

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,39 +19,34 @@ export default function Login() {
         </Container>
       </Navbar>
       <Container>
-        <Row>
-          <div className="d-flex justify-content-center align-items-center vh-100">
-            <Col lg={6} className="d-flex justify-content-center align-items-center">
-              <Image src={login} className="w-100" style={{ minHeight: '50vh' }} />
-            </Col>
-            <Col lg={6} className='bordaformlogin'>
-              <div className="bordaformlogin d-flex flex-column justify-content-center align-items-center">
-                <div className="topoFormLogin p-1">
-                  <h4 className="ms-3 mt-3">Login</h4>
-                </div>
-                <Form className="fullForm p-5">
-                  <Form.Group controlId="formBasicEmail" className='textoformlogin'>
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" className="mb-3 custom-border" />
-                  </Form.Group>
-                  <Form.Group controlId="formBasicPassword" className='textoformlogin'>
-                    <Form.Label>Senha</Form.Label>
-                    <Form.Control type="password" placeholder="Password" className='custom-border' />
-                  </Form.Group>
-                  <h6 className='mt-2 esquecisenha'>Esqueceu sua senha</h6>
-                  
-                  <Row className="mt-5">
-                    <Col>
-                      <Button className="botaocadastrese px-1">Cadastre-se</Button>
-                    </Col>
-                    <Col>
-                      <Button className="botaologin px-2">Login</Button>
-                    </Col>
-                  </Row>
-                </Form>
-              </div>
-            </Col>
-          </div>
+        <Row className='vh-100'>
+          <Col lg={6} className="d-flex justify-content-center align-items-center">
+            <Image src={login} className="w-100" style={{ minHeight: '50vh' }} />
+          </Col>
+          <Col lg={6} className='bordaformlogin d-flex flex-column justify-content-center align-items-center'>
+            <div className="topoFormLogin p-1">
+              <h4 className="ms-3 mt-3">Login</h4>
+            </div>
+            <Form className="fullForm p-5">
+              <Form.Group controlId="formBasicEmail" className='textoformlogin'>
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" className="mb-3 custom-border" />
+              </Form.Group>
+              <Form.Group controlId="formBasicPassword" className='textoformlogin'>
+                <Form.Label>Senha</Form.Label>
+                <Form.Control type="password" placeholder="Password" className='custom-border' />
+              </Form.Group>
+              <h6 className='mt-2 esquecisenha'>Esqueceu sua senha</h6>
+              <Row className="mt-5">
+                <Col>
+                  <Button className="botaocadastrese px-1">Cadastre-se</Button>
+                </Col>
+                <Col>
+                  <Button className="botaologin px-2">Login</Button>
+                </Col>
+              </Row>
+            </Form>
+          </Col>
         </Row>
       </Container>
     </>
