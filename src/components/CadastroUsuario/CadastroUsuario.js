f1dadf8873484ddcca557b9c80c086a841ff3ccf
Vini Black <viniibl4ck@gmail.com>
2023-06-04 21:42:07 -0300

    cadastro de usuario

--- src/components/CadastroUsuario/CadastroUsuario.js
+++ src/components/CadastroUsuario/CadastroUsuario.js
@@ -1,79 +1,94 @@
-import React, { useState } from "react";
-import { Row, Col, Image, Form, Button } from "react-bootstrap";
 
-const CarrinhoPagamento = () => {
-  const [selectedCard, setSelectedCard] = useState("cadastrado");
-  const [newCardInfo, setNewCardInfo] = useState("");
+import { Image, Form, Button, Navbar, Container, Col, Row } from 'react-bootstrap';
 
-  const handleCardChange = (event) => {
-    setSelectedCard(event.target.value);
-  };
-
-  const handleNewCardChange = (event) => {
-    setNewCardInfo(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Lógica para finalizar o pagamento
-  };
-
+export default function CadastroUsuario() {
   return (
-    <Row className="justify-content-center align-items-center">
-      <Col md={6}>
-        <Image src="caminho-da-imagem.jpg" alt="Produto" />
-        <div>
-          <h3>Nome do Produto</h3>
-          <p>Período de Diárias: X dias</p>
-          <p>Valor da Diária: R$X</p>
-          <p>Valor do Seguro: R$X</p>
-          <p>Valor do Caução: R$X</p>
-          <p>Total: R$X</p>
+    <>
+      <Navbar style={{ backgroundColor: '#16697A' }}>
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src="logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="Logo"
+            />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container>
+      <div className="d-flex justify-content-center align-items-center vh-100">
+        <Image src="url_da_imagem" className=" ms-5" style={{ height: '60vh' }} />
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <div className="border rounded-top" style={{ width: '80%', backgroundColor: '#4BBE8F' }} >
+            <h4 className=" ms-3 my-3 text-white" >Dados pessoais</h4>
+          </div>
+          <Form className="border rounded-bottom p-4" style={{ height: '50vh', width: '80%' }}>
+            <Row>
+              <Col md={6}>
+                <Form.Group controlId="formNome">
+                  <Form.Label>Nome</Form.Label>
+                  <Form.Control type="text" placeholder="Digite seu nome" />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group controlId="formSobrenome">
+                  <Form.Label>Sobrenome</Form.Label>
+                  <Form.Control type="text" placeholder="Digite seu sobrenome" />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group controlId="formEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" placeholder="Digite seu email" />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group controlId="formGenero">
+                  <Form.Label>Gênero</Form.Label>
+                  <Form.Control as="select" defaultValue="Selecione">
+                    <option>Selecione</option>
+                    <option>Masculino</option>
+                    <option>Feminino</option>
+                    <option>Não-binário</option>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group controlId="formTelefone">
+                  <Form.Label>Telefone</Form.Label>
+                  <Form.Control type="tel" placeholder="Digite seu telefone" />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group controlId="formNascimento">
+                  <Form.Label>Data de Nascimento</Form.Label>
+                  <Form.Control type="date" placeholder="Digite sua data de nascimento" />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group controlId="formSenha">
+                  <Form.Label>Senha</Form.Label>
+                  <Form.Control type="password" placeholder="Digite sua senha" />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group controlId="formConfirmacaoSenha">
+                  <Form.Label>Confirmação de Senha</Form.Label>
+                  <Form.Control type="password" placeholder="Confirme sua senha" />
+                </Form.Group>
+              </Col>
+            </Row>
+            <div className="d-flex justify-content-between my-5">
+              <div className="ms-auto">
+                <Button className='px-5'style={{ backgroundColor: '#4BBE8F' }}>Continuar</Button>
+              </div>
+            </div>
+          </Form>
         </div>
-      </Col>
-      <Col md={6}>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group>
-            <Form.Label>Forma de Pagamento:</Form.Label>
-            <Form.Check
-              type="radio"
-              label="Cartão Cadastrado"
-              name="paymentMethod"
-              value="cadastrado"
-              checked={selectedCard === "cadastrado"}
-              onChange={handleCardChange}
-            />
-            <Form.Check
-              type="radio"
-              label="Novo Cartão"
-              name="paymentMethod"
-              value="novo"
-              checked={selectedCard === "novo"}
-              onChange={handleCardChange}
-            />
-          </Form.Group>
-          {selectedCard === "cadastrado" && (
-            <Form.Group>
-              <Form.Label>Cartões Cadastrados:</Form.Label>
-              {/* Renderizar os cartões cadastrados aqui */}
-            </Form.Group>
-          )}
-          {selectedCard === "novo" && (
-            <Form.Group>
-              <Form.Label>Novo Cartão:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Informações do novo cartão"
-                value={newCardInfo}
-                onChange={handleNewCardChange}
-              />
-            </Form.Group>
-          )}
-          <Button type="submit">Alugar</Button>
-        </Form>
-      </Col>
-    </Row>
-  );
-};
-
-export default CarrinhoPagamento;
+      </div>
+      </Container>
+    </>
+  )
+}
--- src/components/CadastroUsuario/CadastroUsuario3.js
+++ src/components/CadastroUsuario/CadastroUsuario3.js
@@ -1,72 +1,46 @@
-import { Image, Form, Button, Navbar, Container } from "react-bootstrap";
-import Header from "../Header/Header";
+import { Image, Form, Button, Navbar, Container } from 'react-bootstrap';
 
 export default function CadastroUsuario3() {
   return (
     <>
-      <Header backgroundColor={"#16697A"} />
+      <Navbar style={{ backgroundColor: '#16697A' }}>
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src="logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="Logo"
+            />
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
       <div className="d-flex justify-content-center align-items-center vh-100">
-        <Image
-          src="url_da_imagem"
-          className="mx-5"
-          style={{ height: "70vh" }}
-        />
+        <Image src="url_da_imagem" className="mx-5" style={{ height: '70vh' }} />
         <div className="flex-column">
-          <div
-            className="border rounded-top w-100"
-            style={{ backgroundColor: "#4BBE8F" }}
-          >
+          <div className="border rounded-top w-100" style={{ backgroundColor: '#4BBE8F' }}>
             <h4 className="ms-3 my-3 text-white">Documentação</h4>
           </div>
-          <Form
-            className="border rounded-bottom p-4"
-            style={{ height: "65vh", width: "100%" }}
-          >
+          <Form className="border rounded-bottom p-4" style={{ height: '65vh', width: '100%' }}>
             <Form.Group controlId="formBasicUserPhoto">
               <Form.Label>Foto do usuário (jpg, png ou pdf)</Form.Label>
-              <Form.Control
-                type="file"
-                accept=".jpg,.png,.pdf"
-                className="mb-3"
-                style={{ width: "400px" }}
-              />
+              <Form.Control type="file" accept=".jpg,.png,.pdf" className="mb-3" style={{ width: '400px' }} />
             </Form.Group>
             <Form.Group controlId="formBasicDocumentFront">
               <Form.Label>Documento frente (jpg, png ou pdf)</Form.Label>
-              <Form.Control
-                type="file"
-                accept=".jpg,.png,.pdf"
-                className="mb-3"
-                style={{ width: "400px" }}
-              />
+              <Form.Control type="file" accept=".jpg,.png,.pdf" className="mb-3" style={{ width: '400px' }} />
             </Form.Group>
             <Form.Group controlId="formBasicDocumentBack">
               <Form.Label>Documento verso (jpg, png ou pdf)</Form.Label>
-              <Form.Control
-                type="file"
-                accept=".jpg,.png,.pdf"
-                className="mb-3"
-                style={{ width: "400px" }}
-              />
+              <Form.Control type="file" accept=".jpg,.png,.pdf" className="mb-3" style={{ width: '400px' }} />
             </Form.Group>
             <Form.Group controlId="formBasicResidenceProof">
-              <Form.Label>
-                Comprovante de residência (jpg, png ou pdf)
-              </Form.Label>
-              <Form.Control
-                type="file"
-                accept=".jpg,.png,.pdf"
-                style={{ width: "400px" }}
-              />
+              <Form.Label>Comprovante de residência (jpg, png ou pdf)</Form.Label>
+              <Form.Control type="file" accept=".jpg,.png,.pdf" style={{ width: '400px' }} />
             </Form.Group>
             <div className="d-flex justify-content-between my-5">
-              <Button
-                variant="primary"
-                className="mx-auto px-5"
-                style={{ backgroundColor: "#4BBE8F" }}
-              >
-                Finalizar
-              </Button>
+              <Button variant="primary" className="mx-auto px-5" style={{ backgroundColor: '#4BBE8F' }}>Finalizar</Button>
             </div>
           </Form>
         </div>
