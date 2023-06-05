f1ed9f9edb56a4fb9f5152bc14f8b4683a9adbd0
Gustavo Assis <guu.assis@hotmail.com>
2023-06-05 00:52:02 -0300

    finalizando not found

--- src/components/notFound/NotFound.js
+++ src/components/notFound/NotFound.js
@@ -1,5 +1,27 @@
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
+import './css/NotFound.css'
+
 export default function NotFound(){
     return(
-        <h2 className="text-center mt-5">404 - Página não encontrada</h2>
+        <section > 
+            <header> 
+            <Navbar/>
+            </header>
+
+            <div className="not-found-content d-flex justify-content-center" > 
+                <div className="not-found d-flex align-items-end"> 
+                    <img src="./notfound.jpg"/>
+                </div>
+            </div>
+            <div className="button-content">
+                <div className="d-flex justify-content-center"> 
+                    <button> Inicio</button>
+                </div>
+            </div>
+            <Footer/>
+        </section>
+
+        
     );
 }
