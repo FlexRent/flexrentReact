73c58984adb0ea46813296e058d583d5789484bc
Guilherme <guilima102013@gmail.com>
2023-06-15 15:39:22 -0300

    arrumando

--- src/components/MinhaArea/Perfil/Perfil.js
+++ src/components/MinhaArea/Perfil/Perfil.js
@@ -31,7 +31,7 @@ export default function Carteira() {
                         <div className='estrela-img d-flex justify-content-center align-items-center'>
                             <img src='./assets/minhaArea/star.png' alt='avaliacao'></img>
                         </div>
-                        <span className='avaliacao-res px-3'>{user ? user.assessment.toFixed(2) : "5.0"}</span>
+                        <span className='avaliacao-res px-3'>{user.assessment ? user.assessment.toFixed(2) : "5.0"}</span>
                     </div>
                 </div>
 
